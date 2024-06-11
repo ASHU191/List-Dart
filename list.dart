5186464fcce23e611c24b12d8a48d40b75c08379
ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-06-11 20:12:00 +0500

    Update list.dart

--- list.dart
+++ list.dart
@@ -9,3 +9,42 @@ void main() {
   
   
 }
+
+
+void main() {
+//      List abc=[6,8,2,4,1,5];
+//   var xyz=abc.last;
+//   print(xyz);
+//   print(abc);
+//   abc.sort ();
+//   print(abc);
+//   var bbb = abc.length;
+//   print(bbb);
+//   var aaa = abc.elementAt(2);
+//   print(aaa);
+  
+  
+  List<dynamic>random=
+  [0,2,5,4,1,3.4,'a','b'];
+  print(random);
+  random.add(6);
+     print(random);
+  random.addAll([1,2,5,'aa1']);
+  print(random);
+  var a=random.length;
+  print(a);
+  var b=random.runtimeType;
+  print(b);
+  var c=random.remove(8);
+  print(c);
+  var d=random.contains(3.4);
+  print(d);
+  var f=random.reversed;
+  print(f);
+  var g=random.sort;
+  print(g);
+ random.clear();
+  print(random);  
+  
+  
+}
